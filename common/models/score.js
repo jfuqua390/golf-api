ab5635d79fce06975435304e0b67563bd2055265
Jonathan Fuqua <jfuqua390@gmail.com>
2016-05-06 15:50:06 -0500

    User can add a score

--- common/models/score.js
+++ common/models/score.js
@@ -1,3 +1,11 @@
 module.exports = function(Score) {
+  Score.beforeRemote('create', function(context, user, next) {
+    var req = context.req;
 
+    req.body.data.relationships.golfer.data = {
+      id: req.accessToken.userId,
+    };
+
+    next();
+  });
 };
